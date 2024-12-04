1ccd2a0b4a9bfadf0eee237e3dccb0a425e39dc5
belvip <pouadjeubelvi@gmail.com>
2024-12-04 21:49:51 +0100

    allow the user to select any number from 1 to 10 in the dropdown

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -9,10 +9,7 @@ export const loader = async ({ params }) => {
 };
 
 const SingleProduct = () => {
-
   const { product } = useLoaderData();
-  //console.log(product);
-  
   const { image, title, price, description, colors, company } = product.attributes;
   const fcfaAmount = formatPrice(price);
   const [productColor, setProductColor] = useState(colors[0]);
@@ -35,38 +32,30 @@ const SingleProduct = () => {
         </ul>
       </div>
       {/* PRODUCT */}
-      <div className='mt-6 grid gap-y-8 lg:grid-cols-2  lg:gap-x-16'>
+      <div className='mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16'>
         {/* IMAGE */}
         <img
           src={image}
           alt={title}
-          className='w-96 h-96 object-cover rounded-lg lg:w-full  '
+          className='w-96 h-96 object-cover rounded-lg lg:w-full'
         />
         {/* PRODUCT INFO */}
         <div>
           <h1 className='capitalize text-3xl font-bold'>{title}</h1>
-          <h4 className='text-xl text-neutral-content font-bold mt-2'>
-            {company}
-          </h4>
-
+          <h4 className='text-xl text-neutral-content font-bold mt-2'>{company}</h4>
           <p className='mt-3 text-xl'>{fcfaAmount}</p>
-
           <p className='mt-6 leading-8'>{description}</p>
 
           {/* COLORS */}
           <div className='mt-6'>
-            <h4 className='text-md font-medium tracking-wider capitalize'>
-              colors
-            </h4>
+            <h4 className='text-md font-medium tracking-wider capitalize'>colors</h4>
             <div className='mt-2'>
               {colors.map((color) => {
                 return (
                   <button
                     key={color}
                     type='button'
-                    className={`badge  w-6 h-6 mr-2  ${
-                      color === productColor && 'border-2 border-secondary'
-                    }`}
+                    className={`badge w-6 h-6 mr-2 ${color === productColor && 'border-2 border-secondary'}`}
                     style={{ backgroundColor: color }}
                     onClick={() => setProductColor(color)}
                   ></button>
@@ -77,29 +66,22 @@ const SingleProduct = () => {
           {/* AMOUNT */}
           <div className='form-control w-full max-w-xs'>
             <label className='label'>
-              <h4 className='text-md font-medium tracking-wider capitalize'>
-                amount
-              </h4>
+              <h4 className='text-md font-medium tracking-wider capitalize'>amount</h4>
             </label>
             <select
               className='select select-secondary select-bordered select-md'
               value={amount}
               onChange={handleAmount}
             >
-              <option value={1}>1</option>
-              <option value={2}>2</option>
-              <option value={3}>3</option>
-              <option value={3}>4</option>
-              <option value={3}>5</option>
-              <option value={3}>6</option>
-              <option value={3}>7</option>
-              <option value={3}>8</option>
-              <option value={3}>9</option>
-              <option value={3}>10</option>
+              {[...Array(10)].map((_, index) => (
+                <option key={index + 1} value={index + 1}>
+                  {index + 1}
+                </option>
+              ))}
             </select>
           </div>
           {/* CART BUTTON */}
-          <div className='mt-10 '>
+          <div className='mt-10'>
             <button
               className='btn btn-secondary btn-md'
               onClick={() => console.log('add to bag')}
@@ -111,7 +93,6 @@ const SingleProduct = () => {
       </div>
     </section>
   );
+};
 
-}
-
-export default SingleProduct
+export default SingleProduct;
