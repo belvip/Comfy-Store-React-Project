a569019de1e4879fab90b9da3539e4b1091010fb
belvip <pouadjeubelvi@gmail.com>
2024-12-11 13:31:19 +0100

    Refactor and Local Storage

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -10,10 +10,13 @@ const defaultState = {
     orderTotal: 0,
 };
 
+const getCartFromLocalStorage = () => {
+    return JSON.parse(localStorage.getItem('cart')) || defaultState;
+};
 
 const cartSlice = createSlice({
     name: 'cart',
-    initialState: defaultState,
+    initialState: getCartFromLocalStorage(),
     reducers: {
         addItem: (state, action) => {
             // console.log(action.payload);
@@ -27,9 +30,9 @@ const cartSlice = createSlice({
             }
             state.numItemsInCart += product.amount;
             state.cartTotal += product.price * product.amount;
-            state.tax = 0.1 * state.cartTotal;
-            state.orderTotal = state.cartTotal + state.shipping + state.tax;
-            localStorage.setItem('cart', JSON.stringify(state));
+            
+            cartSlice.caseReducers.calculateTotals(state);
+            
             toast.success('Item added to cart');
 
         
@@ -38,6 +41,12 @@ const cartSlice = createSlice({
     
         removeItem: (state, action) => {},
         editItem: (state, action) => {},
+
+        calculateTotals: (state) => {
+            state.tax = 0.1 * state.cartTotal;
+            state.orderTotal = state.cartTotal + state.shipping + state.tax;
+            localStorage.setItem('cart', JSON.stringify(state));
+        },
     },
 });
 
