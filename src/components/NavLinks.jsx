8c63efdb7e0cd26c68e0d2af1ac3bee0a27ff4e1
belvip <pouadjeubelvi@gmail.com>
2024-12-17 12:47:48 +0100

    Logout and Access UserAfficher le nom le no

--- src/components/NavLinks.jsx
+++ src/components/NavLinks.jsx
@@ -1,7 +1,9 @@
 import { NavLink } from "react-router-dom";
+import { useSelector } from 'react-redux';
+
 
 const NavLinks = () => {
-    //const user = useSelector((state) => state.userState.user);
+    const user = useSelector((state) => state.userState.user);
 
     const links = [
         { id: 1, url: '/', text: 'home' },
@@ -16,7 +18,7 @@ const NavLinks = () => {
         <>
             { links.map((link) => {
                 const {id, url, text } = link;
-                //if ((url === 'checkout' || url === 'orders') && !user) return null;
+                if ((url === 'checkout' || url === 'orders') && !user) return null;
                 return (
                     <li key={id}>
                         <NavLink className='capitalize' to={url}>
